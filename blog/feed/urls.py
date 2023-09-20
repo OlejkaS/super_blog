8c84bf6bf8c7229440dd54ad75e49be84047d841
Олег Силецкий <siletskiyoleg@gmail.com>
2023-09-20 17:45:58 +0300

    add exceptions page

--- blog/feed/urls.py
+++ blog/feed/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+handler404 = 'exceptions.views.page_not_found'
+
+handler500 = 'exceptions.views.server_error'
+
 urlpatterns = [
     path('', views.index, name='index')
 ]
